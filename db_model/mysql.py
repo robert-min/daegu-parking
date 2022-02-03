9c1884cd314541538c29187db4f0b522e29b3183
robert-min <robertmin522@gmail.com>
2022-02-03 23:13:38 +0900

    mysql.py - db와 flask 연결

---
+++ db_model/mysql.py
@@ -0,0 +1,33 @@
+import pymysql
+
+# DAO : Data Access Object DB를 사용해서 데이터를 조회하거나 조작하는 기능을 전담하도록 만든 오브젝트
+class PyMyDao:
+
+
+    def __init__(self):
+        pass
+
+    def getIlParking(self):
+        MYSQL_CONN = pymysql.connect(host='localhost', port=3306, user='root', password='aksen5466!', db='parking', charset='utf8')
+        db_data = []
+        cursor = MYSQL_CONN.cursor()
+
+        sql = "SELECT * FROM illegal"
+        cursor.execute(sql)
+
+        rows = cursor.fetchall()
+        for row in rows:
+            temp = {'address':row[1], 'count':row[2], 'year':row[3], 'long':row[4], 'lat':row[5]}
+            db_data.append(temp)
+
+        MYSQL_CONN.commit()
+        MYSQL_CONN.close()
+        return db_data
+
+# 잘 불러와지는지 태스트 코드
+if __name__ == '__main__':
+    base = PyMyDao()
+    db_data_list = base.getIlParking()
+    print(db_data_list)
+
+
--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+from flask import Flask, render_template
+from db_model.mysql import PyMyDao
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
+@app.route('/html_test')
+def index_html():
+    ilList = PyMyDao().getIlParking()
+    return render_template('index.html', ilList=ilList)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(host='0.0.0.0', port='8080')
